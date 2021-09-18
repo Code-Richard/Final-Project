4ef36b4d069377ea2834d8d2bfe1e259af5cd30e
Richard <rademuwagun@gmail.com>
2021-09-18 21:52:45 +0100

    Added route for read later

--- app.py
+++ app.py
@@ -7,49 +7,69 @@ import re
 
 app = Flask(__name__)
 articles_list = []
+read_later = []
 tickers = set()
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    
-    # Creates a list of empty dictionaries to store article data on each stock
-    for ticker in tickers:
-        # Gets page for specific article
-        url = f"https://finance.yahoo.com/quote/{ticker}"
-        result = requests.get(url)
-        doc = BeautifulSoup(result.text, "html.parser")
+    if request.method == "GET":
+        for ticker in tickers:
+            # Gets page for specific article
+            url = f"https://finance.yahoo.com/quote/{ticker}"
+            result = requests.get(url)
+            doc = BeautifulSoup(result.text, "html.parser")
+            
+            # Finds articles in Yahoo Finance Page
+            articles = doc.findAll(class_="js-stream-content Pos(r)")
+
+            # Stores info about each article in articles_list
+            for count, article in enumerate(articles, start=(len(articles_list))):
+                # Accessing HTML elements
+                temp_dict = {}
+                source = article.find(class_="C(#959595) Fz(11px) D(ib) Mb(6px)")
+                link = article.find(href=True).get('href')
+                title = source.next_sibling
+                description = title.next_sibling
+
+                # Adding article to dictionary
+                temp_dict['stock'] = ticker
+                temp_dict['title'] = title.text
+                temp_dict['description'] = description.text
+                temp_dict['source'] = source.text
+                temp_dict['link'] = url+link
+                print(count)
+                temp_dict['id'] = count
+
+                # Adding dictionary to list
+                articles_list.append(temp_dict.copy())
         
-        # Finds articles in Yahoo Finance Page
-        articles = doc.findAll(class_="js-stream-content Pos(r)")
+        #Randomises feed
+        shuffle(articles_list)
 
-        # Stores info about each article in articles_list
-        for article in articles:
-            temp_dict = {}
-            source = article.find(class_="C(#959595) Fz(11px) D(ib) Mb(6px)")
-            link = article.find(href=True).get('href')
-            title = source.next_sibling
-            description = title.next_sibling
-
-            # Adding article to dictionary
-            temp_dict['stock'] = ticker
-            temp_dict['title'] = title.text
-            temp_dict['description'] = description.text
-            temp_dict['source'] = source.text
-            temp_dict['link'] = url+link
-
-
-            # Adding dictionary to list
-            articles_list.append(temp_dict.copy())
-    
-    shuffle(articles_list)
-
-    return render_template("index.html", articles=articles_list)
+        return render_template("index.html", articles=articles_list)
+    else:
+        # Adds article to read_later list
+        article_number = int(request.form.get('article_id'))
+        for article in articles_list:
+            if article['id'] == article_number:
+                print(article['id'])
+                read_later.append(article)
+            
+            
+        return redirect("/readlater")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
+    # Adds users stock into feed
     if request.method == "POST":
-        stock = request.form.get('stock')
+        stock = request.form.get('stock').upper()
         tickers.add(stock)
         return redirect("/add")
     else:
         return render_template("add.html")
+
+
+@app.route("/readlater")
+def readlater():
+    return render_template("readlater.html", read_later=read_later)
+        
